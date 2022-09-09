97e91552e63121917c50df8480971e8c2ef8a17b
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-08 18:35:38 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - ENtry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcme is a piece of art,\n");
+	return (0);
+
+
+}
