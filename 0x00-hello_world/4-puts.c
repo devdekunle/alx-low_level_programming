108ae256620c60e2806939bc85e80696464cfcd9
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-08 17:00:48 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return Always 0 (Success)
+ *
+ * Program to print to sdout using puts
+ */
+int main(void)
+
+{
+	char string[] = "Programming is like building a multilingual puzzle";
+
+	puts(string);/*printing string to output*/
+	return (0);
+
+}
