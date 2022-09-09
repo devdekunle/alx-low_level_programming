a7eeebd38ed9ead73285b6665724cd494f63bf15
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-08 17:51:12 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 int main(void)
 
 {
-	char string[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(string);/*printing string to output*/
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
