00771d0f0fb6032594c54c8f0a4964e2314d3aaa
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-09 17:49:51 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int num = 0;
+
+	int newLine = '\n';
+
+	while (num < 10)
+	{
+		putchar(48 +  num);
+		num++;
+	}
+	putchar (newLine);
+	return(0);
+}
