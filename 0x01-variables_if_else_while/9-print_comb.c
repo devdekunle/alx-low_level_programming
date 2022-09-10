8bcd8d81d79202b391260acd80f6aa5c9ed60c3c
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-09 19:08:21 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main()
+
+{
+	int num1 = 0;
+
+	int newLine = '\n';
+	
+	while (num1 < 10)
+	{
+		putchar(48 + num1);
+
+		if (num1 != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	num1++;
+	}
+	putchar(newLine);
+	return (0);	
+}
