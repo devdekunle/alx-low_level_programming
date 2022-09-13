4bbaf96d685c8bebe18d25cd8f9c41cb4e228dec
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 09:38:23 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,18 @@
  * Return: Always 0
  */
 int main(void)
-{	
+
+{
 	int i = 0;
+
 	char ch[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
 	while (i < 8)
 	{
 		_putchar(ch[i]);
 		i++;
-	
-	
+
+
 	}
 	_putchar('\n');
 	return (0);
