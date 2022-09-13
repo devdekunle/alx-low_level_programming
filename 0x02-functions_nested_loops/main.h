593c6dec9106dfaba7371f5a3ab9b6bbaef93ebc
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 09:30:59 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,7 @@
-#include<unistd.h>
-/**
- * _putchar-write the character c to stdout
- * @c: the character to print
- *
- * Return:on success 1
- *
- * on error, -1 is returned, and errno is set appropriately
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));	
 
-}
+int _putchar(char);
+
+#endif
