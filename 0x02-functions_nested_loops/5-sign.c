74f69aa084cea221213fc09ba9f33acc6636547b
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 16:03:05 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,28 @@
 #include "main.h"
 /**
- * 
+ *print_sign - prints the sign of a number
+ *@n: check the sign of a number
+ *Return: 1, 0 or -1
+ */
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		test = 1;
+	}
+	else if (n == 0)
+	{
+
+		_putchar('0');
+		test = 0;
+	}
+	else
+	{
+		_putchar('-');
+		test = -1;
+	}
+	return (test);
+}
