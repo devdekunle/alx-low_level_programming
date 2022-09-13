b7a41eaa50fbac2d692351b7a0355dd9c58cf953
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 13:17:30 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print multiple alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	print_alphabet(void);
+	putchar('\n');
+}
