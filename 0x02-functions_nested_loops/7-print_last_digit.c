e82a65d8fd976211a00a19f59b5b767e50af4d83
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 16:53:57 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,16 @@ int print_last_digit(int n)
 {
 	int value;
 
-	value = n % 10;
+	if (n < 0)
+	{
+		value = -1 * (n % 10);
+	}
+	else
+	{
 
+	value = n % 10;
+	}
+	_putchar(value + '0');
 	return (value);
 
 }
