6b517f240064bd6a06d7134904f6a997ca3008af
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 16:23:39 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _abs - get absolute value of a number
- * @int: number to be made absolute
+ * @n: number to be made absolute
  * Return: 0
  */
 
-int _abs(int)
+int _abs(int n)
 {
 	return (n < 0 ? n : n * -1);
 
