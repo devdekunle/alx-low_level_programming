9505bfbb1720ea8ab5b6b3893bec516ef33bd549
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-14 14:58:38 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,7 @@ void print_times_table(int n)
 					_putchar(num / 10 + '0');
 					_putchar(num % 10 + '0');
 				}
-				else
+				else if  (num > 0 && num < 10)
 				{	_putchar(' ');
 					_putchar(num + '0');
 				}
