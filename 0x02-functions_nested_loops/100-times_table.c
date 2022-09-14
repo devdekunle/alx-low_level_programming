081d36369172375e4eb943f6a9d3fd212657ebb8
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-14 06:17:28 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,11 +8,11 @@ void print_times_table(int n)
 {
 	int i, j, num;
 
-	if (!(n > 15 || n < 0))
+	if (n < 15 || n > 0)
 	{
-		for  (i = 0; i < n; i++)
+		for  (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				num = i * j;
 				if (j == 0)
