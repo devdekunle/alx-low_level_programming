4620342f613d8f7e1db9af5a97180196d022ecc0
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 19:33:25 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add numbetrs
+ * @num1: first number
+ * @num2: second number
+ * Return: the sum of two numbers
+ */
+int add(int num1, int num2)
+{
+
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+
+}
