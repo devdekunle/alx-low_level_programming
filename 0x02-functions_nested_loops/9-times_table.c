01c4bfa9272a12bf40e8ce747b22bb91abe19727
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-13 19:22:20 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,7 @@
 void times_table(void)
 
 {
-	int i;
-
-	int j;
-
-	int num;
+	int i, j, num;
 
 	i = 0;
 	while (i < 10)
@@ -28,7 +24,6 @@ void times_table(void)
 			{
 				_putchar(num / 10 + '0');
 				_putchar(num % 10 + '0');
-
 			}
 			else
 			{
@@ -36,11 +31,10 @@ void times_table(void)
 				_putchar(num + '0');
 			}
 
-			if ( j< 9)
+			if ( j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-
 			}
 
 			j++;
@@ -48,6 +42,4 @@ void times_table(void)
 		_putchar('\n');
 		i++;
 	}
-
-
 }
