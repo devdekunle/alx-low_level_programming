c4ffa6d352fdb507e0210722ce04a3812e621986
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-15 04:16:03 -0700

    task 2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints the largest of three numbers
+* Return: Always 0
+*/
+
+int main()
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b =  -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	return (largest);
+
+}
+
