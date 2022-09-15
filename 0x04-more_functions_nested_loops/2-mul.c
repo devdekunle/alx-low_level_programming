9675c30cc59cfc2487d4508ac38a9393f6bd4de0
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-15 15:06:38 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _mul - multiply two numbers
+*@a: first number
+*@b: second number
+*Return: value
+*/
+
+int mul(int a, int b)
+{
+	int value;
+
+	value = a * b;
+
+	return (value);
+}
