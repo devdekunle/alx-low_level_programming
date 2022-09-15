c8553942a1ad5edfe98944e1b4c9c98ffc690928
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-15 15:58:32 -0700

    task 5 && git push

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* more_numbers - print more numbrs
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i, num, num2;
+
+	while (i < 10)
+	{
+		num = 0;
+		while (num <= 14)
+		{
+			num2 = num;
+
+			if (num > 9)
+			{
+				_putchar('1');
+				num2 = num % 10
+			}
+		_putchar(num2 + '0');
+		num++;
+		}
+
+		i++;
+	}
+}
