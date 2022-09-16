ad145a081ed2eb7f6ed16f6e595e854c61c80221
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-16 16:02:52 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_square - check for a digit
- * @n : number of _ to be printed
- * Return:void
- */
+* print_square - check for a digit
+* @n : number of _ to be printed
+* Return:void
+*/
 
- void print_square(int n)
- {
+void print_square(int n)
+{
 	int i = 0, ii;
 
 	while (i < n && n > 0)
@@ -25,7 +25,6 @@
 	}
 	if (i == 0)
 		_putchar('\n');
-	
 
- }
+}
 
