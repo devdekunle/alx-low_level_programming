e1903b7fad31bf3f99ff1a132338e4200a9aea26
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-19 12:38:55 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap - exchange values of two numbers
+*@a: first pointer
+@b: second pointer
+*Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,7 @@
 #define MAIN_H
 void reset_to_98(int *n);
 int _putchar(char);
-
-
+void swap_int(int *a, int *b);
 
 
 
