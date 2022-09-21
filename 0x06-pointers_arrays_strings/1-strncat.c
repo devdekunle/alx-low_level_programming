488f9f4fd221bc0a0a68ef887dd433719d8ee752
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-21 15:17:21 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
-*_strcat - concatenate two strings
+*_strncat - concatenate two strings
 *@src: string to append from
 *@dest: string to append to
+*@n: number of bytes before loop termination
 *Return: dest
 */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int destLen = _strlen(dest);
 	int i;
 
-	for (i = 0; src[i]; i++)
+	for (i = 0; src[i] < n; i++)
 	{
 		dest[i + destLen] = src[i];
 
