819eec876d4c7827481698157c2189e40c59ed92
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-20 17:56:07 -0700

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*_atoi - convert string to integer
+*@s: string to chang
+*Return: 0
+*/
+
+int _atoi(char *s)
+{
+	int startVal, i, num, sign = -1;
+
+	for (startVal = 0; !(s[startVal] >= 48 && s[startVal] <= 57); startVal++)
+	{
+		if (s[startVal] == "-")
+		{
+			sign = sign * -1;
+		}
+	}
+
+	for (i = startVal; (s[i] >= 48 && s[i] <= 57); i++)
+	{
+
+		num = num * 10 + (s[i] - 48);
+
+	}
+	return (num * sign);
+
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 void reset_to_98(int *n);
 int _putchar(char);
+int _atoi(char *s);
 void swap_int(int *a, int *b);
 char *_strcpy(char *dest, char *src);
 void print_rev(char *s);
