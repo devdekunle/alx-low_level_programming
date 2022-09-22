2e81d0549a8eb3a0fd44dca6b7adb8bf84b5a535
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-22 15:37:51 -0700

    task 6

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *string_toupper(char *);
+char *string_toupper(char *n);
+char *cap_string(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
