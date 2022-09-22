ac97f9b53313d2f4e1d1782a6096a8edbb68b1f4
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-22 14:04:37 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+*reverse_array - program to revers array
+*@a: array to reverse
+*@n: length of array
+*Return: nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+	int *temp[];
+
+	for (i = 0; *(a + i) < n; i++)
+	{
+		*(temp + n) = *(a + i);
+		n--;
+
+	}
+
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
