5a911efdcff70a848cbc5537a41cced13bb3a40f
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-23 17:32:57 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+*rot13 - rotate string according to ceaser cipher
+*@str: string to encode
+*Return: str
+*/
+
+char *rot13(char *str)
+{
+	int i;
+	/*iterate through string and also check if character is an alphabet*/
+	for (i = 0; str[i], (str[i] >= 'a' && str[i] <= 'z') ||
+	(str[i] >= 'A' && str[i] <= 'Z' ++i))
+	{
+
+		/*check if character is between first 13 characters*/
+		if (str[i] >= 'a' && str[i] <= 'm' || str[i] >= 'A' && str[i] <= 'M')
+		{
+
+			/*add 13 to get the character 13 places after it*/
+			str[i] += 13;
+
+
+		}
+		else
+		{
+				/*if not, subtract 13 to get the character 13 places befor it*/
+				str[i] -= 13;
+		}
+
+	}
+
+		return (str);
+
+
+}
