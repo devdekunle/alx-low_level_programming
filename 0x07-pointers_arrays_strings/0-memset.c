c524f2a5238cd7d4e611f64606b9d5defb28658a
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-26 13:59:59 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _memset - fill buffer with character
+*@s: pointer to memory to be filled
+*@b: constant to fill buffer
+*@n: number of bytes to fill buffer
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	for (i = 0; i < n; i++)
+	{
+
+		if (i % 10)
+			_putchar(" ");
+		if (!(i % 10) && i)
+			_putchar('\n');
+		*(s + i) = 'b';
+
+	}
+	_putchar('\n');
+return (s);
+
+
+}
