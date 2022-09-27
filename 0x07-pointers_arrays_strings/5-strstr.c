b3eff8867db46fd665f2355323f2496c0b5f1964
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-27 11:25:12 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,11 +2,28 @@
 /**_strstr - locate  substring within a string
 *@needle: string to locate
 *@heystack: string to check for substring
-*@Return: needle
+kjj*@Return: needle
 */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int i, j, c = i;
+	char *word;
+	for (i = 0; *(haystack + i); i++)
+	{
+		for (j = 0; *(needle + j); j++)
+		
+		{
+			if (*(haystack + i) == *(needle + j))
+			{
+					*(word + c) = *(haystack + i);
+					c++;
 
+					return(word);
+				
+			}
+		}
 
+	}
+
+	return('\0');
 }
