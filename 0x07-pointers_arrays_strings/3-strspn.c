e0cebb097cb892036c87cf635270a1fe6af43973
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-27 09:06:29 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "2-strlen.c"
+
 /**
 *_strspn - function to calculate null byte of a segmentin a string
 *@s: string to check
@@ -10,9 +10,9 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count = 0;
-	
 
-	for (i = 0; *(s + i ) >= '\0'; i++)
+
+	for (i = 0; *(s + i) >= '\0'; i++)
 	{
 		for (j = 0; *(accept + j) > '\0'; j++)
 		{
