4db984efea458b7acd4d4c8fb9426994d021372e
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-28 12:35:36 -0700

    task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
@@ -12,4 +13,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+int _strlen(char *s);
 #endif
