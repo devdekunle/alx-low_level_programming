ac530dc342f3de6afdde9229babfaacf352ebd48
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-30 19:28:05 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry
+*@argc: number of arguments
+*@argv: pointer array to arguemnts
+*Return: 0
+*/
+int main(int argc, char *argv[])
+
+{
+	int i;
+	for (i = 0; i < argc, i++)
+	{
+		printf("%d\n", argv[argc]);
+
+	}
+	return (0);
+
+
+}
