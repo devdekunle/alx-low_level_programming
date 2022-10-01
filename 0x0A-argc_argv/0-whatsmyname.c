2054c0f9fabed63d288b2c34ae6e44eef8ab6d73
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-09-30 19:19:50 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 /**
 *main - Entry
 *@argc:number of arguments passed
-*@:argv: array of pointers to the arguements
+*@argv: array of pointers to the arguements
 *Return: 0
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 
 
 }
