0f37c1a16001828d8a84b4ce59bd57ae0d1986fc
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-03 16:52:42 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,6 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	int i;
 
-	if (size == 48)
-		status = NULL;
-
 	ptr = (char *)malloc(size * sizeof(char));
 
 	if (ptr != NULL)
@@ -22,13 +19,11 @@ char *create_array(unsigned int size, char c)
 		for (i = 0; *(ptr + i) < size; i++)
 
 			*(ptr + i) = c;
-		status = ptr;
+		free(ptr);
+		return (ptr);
 	}
 	else
-		status NULL;
+		return (NULL);
 
-	return (status);
+	return (0);
 }
-
-
-
