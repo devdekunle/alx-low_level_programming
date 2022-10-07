39cc056be23ee5c1663d7e05989ed488940b3a0b
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-07 03:14:27 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,4 @@
-#include "main.h"
+/*#include "main.h"*/
 #include<stdlib.h>
 /**
 *malloc_checked - check malloc status
@@ -11,7 +11,7 @@ void *malloc_checked(unsigned int b)
 
 	ptrMalloc = malloc(b);
 	if (ptrMalloc == NULL)
-		return ("98");
+		exit(98);
 	else
 		return (ptrMalloc);
 
