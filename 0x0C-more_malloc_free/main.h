42bcef7a9694d1eebeacc5ce7b512082b6b40f09
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-07 04:03:30 -0700

    task 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,5 @@ char *create_array(unsigned int size, char c);
 int _putchar(char);
 char *_strdup(char *str);a
 int _strlen(char *s);
-char *str_concat(char *s1, char *s2);
+char *str_nconcat(char *s1, char *s2 unsigned int n);
 #endif
