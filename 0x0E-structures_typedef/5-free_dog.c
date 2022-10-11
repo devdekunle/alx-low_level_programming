4b6e1e1c7a8b80508427438be10ce13c334b9dea
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-11 09:32:49 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+*free_dog - free dogs
+*@d: struct variable to free members
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->age);
+
+	}
+
+
+}
