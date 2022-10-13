c2792890b24d648b770326ee64fc803a1a083737
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-13 06:11:27 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,14 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+/**
+* print_name - print name from a function
+*@name: name to print
+*@f - address of function
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL)
+	if (f != NULL)
 		f(name);
 
 
