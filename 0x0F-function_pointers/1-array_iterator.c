3840008c8bd2d2fc876001aef6a410d6a5614e7b
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-13 06:39:58 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
 *@size: size of the array
 *@action: address of function
 */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, int size, void (*action)(int))
 {
 	int i;
 
