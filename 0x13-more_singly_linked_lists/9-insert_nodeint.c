ea376994b2eabb84a43172ca82ad83530234259c
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-25 20:36:50 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		ptr->next = *head;
-		*head = new_ptr;
+		*head = ptr;
 		return (ptr);
 	}
 	ptr->next = temp->next;
