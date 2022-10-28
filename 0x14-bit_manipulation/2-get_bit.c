0bc42beb5949f2d7035af12750de38546f103616
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-28 04:59:55 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*get_bit - get the value of a bit at an index
+*@n: decimal check
+*@index:index of value to return
+*Return: index or -1
+*/
+int get_bit(unsigned long int n, unsigned int index);
+{
+	int bit;
+	/*shift numbers to the right by amount of index*/
+	bit = (n >> index);
+	if (index > 32)
+		return (-1);
+	return (bit & 1);
+
+
+
+}
