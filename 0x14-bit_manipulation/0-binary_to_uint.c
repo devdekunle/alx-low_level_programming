7f0ace90c204031c50ddb90314731983f437b337
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-10-27 21:13:23 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len = _strlen(b);
 	int num = 0;
 	int i, decVal = 1;
-
-	if (b == NULL)
-		return (0);
-	for (i = (len - 1); i >= 0; i--)
-	{
-		if (b[i] < 48 || b[i] > 49)
+	
+		if (b == NULL)
 			return (0);
-		if (b[i] == 49)
-			num += decVal;
-		decVal *= 2;
+		for (i = (_strlen(b) - 1); i >= 0; i--)
+		{
+			if (b[i] < 48 || b[i] > 49)
+				return (0);
+			if (b[i] == 49)
+				num += decVal;
+			decVal *= 2;
+		}
+		return (num);
 
-	}
-	return (num);
 
-
-}
+}	
