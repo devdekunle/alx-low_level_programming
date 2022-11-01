6494cac75ae5f15fc281234bde458c8f066d1acd
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-11-01 16:47:06 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 
 	if (!text_content)
-		return (-1);
+		return (1);
 	if (!filename)
 		return (-1);
 		/*open file and set to append */
