606222dcae97c1cf71952e9f713982d964162bc5
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-12-08 12:53:25 -0800

    print number of elements of a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num;
+
+	for (num = 0; h != NULL; num++)
+		h = h->next;
+	return (num);
+}
