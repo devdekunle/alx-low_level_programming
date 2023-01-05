adb348209ff23c40229f79b3f775b965efb11d24
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-05 15:06:54 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ptr_table = NULL;
 
 	if (isdigit((char)size) == '0')
-	return (NULL);
+		return (NULL);
 	ptr_table = malloc(sizeof(hash_table_t));
 	if (ptr_table == NULL)
-	return (NULL);
+	{
+		free(ptr_table)
+		return (NULL);
+	}
 	ptr_table->size = size;
 	/*allocate memory for pointer to hash_node pointers*/
 	ptr_table->array = malloc(sizeof(hash_node_t) * ptr_table->size);
+
 	if (ptr_table->array == NULL)
-	return (NULL);
+	{
+		free(ptr_table->array)
+		return (NULL);
+	}
 	/*assign each pointer in the array of pointers to NULL*/
 	for (i = 0; i < ptr_table->size; i++)
 	{
-	ptr_table->array[i] = NULL;
+		ptr_table->array[i] = NULL;
 
 	}
 	return (ptr_table);
