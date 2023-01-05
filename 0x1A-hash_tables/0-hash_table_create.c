309da0278eb6091abc5a95febd5f4994cc0db693
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-05 14:51:21 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+*hash_table_create - create a new hash table 
+*@size: size of the table
+*Return: pointer to created table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    unsigned long int i;
+    hash_table_t *ptr_table = NULL;
+
+    if (isdigit((char)size) == '0')
+        return (NULL);
+    ptr_table = malloc(sizeof(hash_table_t));
+    if (ptr_table == NULL)
+        return (NULL);
+    ptr_table->size = size;
+    /*allocate memory for pointer to hash_node pointers*/
+    ptr_table->array = malloc(sizeof(hash_node_t) * ptr_table->size);
+    if (ptr_table->array == NULL)
+        return (NULL);
+    /*assign each pointer in the array of pointers to NULL*/
+    for (i = 0; i < ptr_table->size; i++)
+    {
+        ptr_table->array[i] = NULL;
+
+    }
+    return (ptr_table);
+
+}
+
+
+
---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,45 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* struct hash_node_s - Node of a hash table
+*
+*@key: The key, string
+*The key is unique to the HashTable
+*@: The value corresponding to a key
+*#next: a pointer to the next node of the list
+*/
+typedef struct hash_node_s
+{
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_t;
+
+/**
+* struct hash_table_s - Hash table data structute
+*
+*@size: The size of the array
+*@array: An array of of size @size
+* Each cell of this array is a pointer to the first nod of a linked list,
+because we eant out HasjTable to use a Chaining collision handling
+*/
+typedef struct hast_table_s
+{
+    unsigned long int size;
+    hash_node_t **array;
+
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+
+
+
+
+
+#endif
