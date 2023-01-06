b137b274e79ae8693b9036d7920e69dae429a71e
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-06 13:10:50 -0800

    fixed st table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL;
 	hash_node_t *new_node = NULL;
 
-	if (ht == NULL || *key == '\n' || *value == '\n')
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	/*assign pointer at index to another pointer*/
