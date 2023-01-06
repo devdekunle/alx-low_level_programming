fb720784aaa771d9b1f0ae3dc135d947b07d804b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-05 16:54:22 -0800

    write hash function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,19 +1,21 @@
 #include "hash_tables.h"
 /**
 *key_index - gets the index of the key in the hash table
-*@key - key to manipulate
-*@size - size of the hash table
-*Return - index
+*@key: key to manipulate
+*@size: size of the hash table
+*Return: index
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int num;
+	unsigned long int index, hash;
 
-	if ((int)key < 48  || (int)size < 48)
-		Return NULL
-	if 
-	
+	if (isdigit((int)size) == '0' || (int)size <= 0 || key == NULL)
+		return (1);
+	/*call hash_djb2 function to get hash value*/
+	hash = hash_djb2(key);
 
-
+	/*get index from hash and table size*/
+	index = hash % size;
+	return (index);
 
 }
