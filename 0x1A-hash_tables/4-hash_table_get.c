43c64f46348146c37a28077c7f0d8e28c30c7f54
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-06 12:36:51 -0800

    get value from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+*hast_table_get - get a value from a key
+*@ht: table to search
+*@key: key being looked for
+*Return: value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	hash_node_t *ptr;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (!index)
+		return (NULL);
+	ptr = ht->array[index];
+	while(ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return ptr->value;
+		ptr = ptr->next;
+	}
+
+	return NULL;
+
+
+}
