8be476ccd5cb7677d21a494d26f063aada3f1d00
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-06 13:29:30 -0800

    fixed create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *ptr_table = NULL;
 
-	if (!size)
+	if (size < 0)
 		return (NULL);
 
 	ptr_table = malloc(sizeof(hash_table_t));
