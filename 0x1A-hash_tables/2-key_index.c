ce2bd60c9e9dc59aa3c95de986291039923b9be1
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-05 16:11:46 -0800

    fixed error

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+*key_index - gets the index of the key in the hash table
+*@key - key to manipulate
+*@size - size of the hash table
+*Return - index
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int num;
+	
+
+
+
+}
