c05e273d65ac5db254def7e3ff2d1e2c9e45af05
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-13 19:09:39 +0100

    time complexity of a linear search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+/**
+*binary_search - search for a value in a sorted array using binary search
+*@size: size of the array to search
+*@value: value to search for in the array
+*Return: index of array of -1
+**/
+
+int binary_search(int *array, size_t size, int value)
+{
+	
+
+
+
+}
