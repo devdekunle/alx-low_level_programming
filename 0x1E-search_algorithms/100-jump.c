d2c716120896430581ddf2415275502e8a72872d
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-16 10:58:06 +0100

    find an element in a list using jump search algo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,10 +7,35 @@
 *@value: value to search for
 *Return: -1 or index of the first instance of the value
 */
-
 int jump_search(int *array, size_t size, int value)
 {
-	
+	int index, block, increment, prev;
 
+	if (array == NULL || size == 0)
+		return (-1);
 
+	block = (int)sqrt((double)size);
+	increment = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		increment++;
+		prev = index;
+		index = increment * block;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
 }
